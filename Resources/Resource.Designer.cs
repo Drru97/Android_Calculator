d2d746fa25a744db2d9a71bf7ea3dc97ff0f11dd
Oleg Dutsyak <oleg.dystak@gmail.com>
2016-09-23 15:59:59 +0300

    First commit

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -63,6 +63,39 @@ namespace Calculator
 			// aapt resource value: 0x7f050000
 			public const int MyButton = 2131034112;
 			
+			// aapt resource value: 0x7f050006
+			public const int button1 = 2131034118;
+			
+			// aapt resource value: 0x7f050003
+			public const int button2 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int button3 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int button4 = 2131034117;
+			
+			// aapt resource value: 0x7f050008
+			public const int button5 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int button6 = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int button7 = 2131034122;
+			
+			// aapt resource value: 0x7f050001
+			public const int tableLayout1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int tableRow1 = 2131034114;
+			
+			// aapt resource value: 0x7f050007
+			public const int tableRow2 = 2131034119;
+			
+			// aapt resource value: 0x7f05000b
+			public const int tableRow3 = 2131034123;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -78,6 +111,9 @@ namespace Calculator
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int tLayout = 2130903041;
 			
 			static Layout()
 			{
